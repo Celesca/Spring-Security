81ce08973cfb903d186852cf80aba94009b06949
Sawit Koseeyaumporn | Folk <folk.sawit@gmail.com>
2024-02-12 02:01:38 +0700

    Implementing Authorization

--- src/main/java/com/example/securitycourse/controller/MemberController.java
+++ src/main/java/com/example/securitycourse/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.example.securitycourse.controller;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     // permission MEMBER_READ
+    @PreAuthorize("hasAuthority('MEMBER_READ') or hasRole('ADMIN')")
     @GetMapping("")
     public String getMember() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
         return "Member resource";
     }
 
--- src/main/java/com/example/securitycourse/securityconfig/SecurityConfig.java
+++ src/main/java/com/example/securitycourse/securityconfig/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.securitycourse.securityconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,19 +16,24 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 //        http.formLogin(withDefaults());
 @Component
 @EnableWebSecurity // Handle Security Config
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/public/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/member/**").hasAuthority("MEMBER_READ")
-                        .requestMatchers(HttpMethod.PUT, "/member/**").hasAuthority("MEMBER_UPDATE")
+//                        .requestMatchers("/member/**").hasAnyRole("MEMBER", "ADMIN")
+                        .requestMatchers("/admin/**").hasAnyRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new ApiKeyAuthFilter(), BasicAuthenticationFilter.class)
@@ -42,12 +49,23 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        UserDetails user = User.withUsername("member")
+
+        CustomUserDetail user = new CustomUserDetail("member", encoder.encode("password"));
+        user.setRoles(List.of("MEMBER"));
+        user.setPermissions(List.of("MEMBER_READ"));
+
+//        UserDetails user = User.withUsername("member")
+//                .password(encoder.encode("password"))
+//                .roles("MEMBER") // Spring Collect -> ROLE_MEMBER
+//                .authorities("MEMBER_READ")
+//                .build();
+
+        UserDetails admin = User.withUsername("admin")
                 .password(encoder.encode("password"))
-                .authorities("MEMBER_READ", "MEMBER_UPDATE")
+                .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
